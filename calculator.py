8e1c0178f55596d9512a43f5d0a26a7e17d6a3da
Yacov Finn <yacovfinn@gmail.com>
2025-01-06 15:15:27 +0200

    Create calculator with add and subtract functions

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# This is the calculator   
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+print("Simple calculator")
+print("Addition (5 + 3):", add(5,3))
+print("Subtraction (10 - 4):", subtract(10,4))
