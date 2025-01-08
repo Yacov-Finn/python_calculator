c636d0de79f547c48a5c3eb20ad69f0fe0f70ff3
Yacov Finn <yacovfinn@gmail.com>
2025-01-08 14:49:05 +0200

    Add multiplication feature

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ if __name__ == "__main__":
     print("Simple calculator")
     print("Addition (5 + 3):", add(5,3))
     print("Subtraction (10 - 4):", subtract(10,4))
+    print("Multiplication (2 * 3):", multiply(2,3))
+
+def multiply(a, b):
+    return a * b
