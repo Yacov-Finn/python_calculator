accf37a62cb60d186c25513a2a70dc40d53e5ebe
Yacov Finn <yacovfinn@gmail.com>
2025-01-12 14:11:38 +0200

    Create multiplication function

--- calculator.py
+++ calculator.py
@@ -5,11 +5,11 @@ def add(a,b):
 def subtract(a,b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Simple calculator")
     print("Addition (5 + 3):", add(5,3))
     print("Subtraction (10 - 4):", subtract(10,4))
     print("Multiplication (2 * 3):", multiply(2,3))
-
-def multiply(a, b):
-    return a * b
